5139dfbcce1b4bdde7765c4528bb021abaeb7f02
Swatantra Prakash <swatantra.prakash@karexpert.com>
2024-03-01 17:13:26 +0530

    Commit

--- src/main/java/com/Eshop/io/eshop/models/BaseModel.java
+++ src/main/java/com/Eshop/io/eshop/models/BaseModel.java
@@ -12,44 +12,4 @@ public class BaseModel {
     private Date modifiedOn;
     public String createdBy;
     private String modifiedBy;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public Date getCreatedOn() {
-        return createdOn;
-    }
-
-    public void setCreatedOn(Date createdOn) {
-        this.createdOn = createdOn;
-    }
-
-    public Date getModifiedOn() {
-        return modifiedOn;
-    }
-
-    public void setModifiedOn(Date modifiedOn) {
-        this.modifiedOn = modifiedOn;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public String getModifiedBy() {
-        return modifiedBy;
-    }
-
-    public void setModifiedBy(String modifiedBy) {
-        this.modifiedBy = modifiedBy;
-    }
 }
